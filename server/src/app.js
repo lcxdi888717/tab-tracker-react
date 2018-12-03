841d203431d60f1641dd916adf2ab233b237c46e
LoveYancy <lcxdi888717@gmail.com>
2018-12-03 22:32:03 +1100

    Add server side simple handler to register api

--- server/src/app.js
+++ server/src/app.js
@@ -3,8 +3,17 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 
-const app = express();
 const port = 8081;
+const app = express();
+
+app.use(bodyParser.json());
+
 app.get("/status", (req, res) => res.send("Hello World!"));
+
+app.post("/register", (req, res) =>
+  res
+    .status(400)
+    .send(`Receive Email: ${req.body.email} & Password: ${req.body.password}`)
+);
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
